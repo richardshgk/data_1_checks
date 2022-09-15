62c6e69ededd6fa25e1d24e9f22c9e567caacfed
Steve Jones <jones.wiredbraincoffee@gmail.com>
2022-09-14 20:12:13 -0400

    first commit

---
+++ data_check_9.14.py
@@ -0,0 +1,7 @@
+print("Hello world!")
+fruits = ["apples", "oranges", "kiwi", "rasperries"]
+print(fruits[2])
+dirctionary = {"one" : "item one", "two" : "item two"}
+print(dirctionary["one"])
+grades = ("first", "second", "third", "fourth", "fifth")
+print(grades[3])
