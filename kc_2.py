865bcb115771ed181feda47771d626666b6db318
Steve Jones <jones.wiredbraincoffee@gmail.com>
2022-11-05 11:43:37 -0400

    created bar chart

--- kc_2.py
+++ kc_2.py
@@ -18,9 +18,17 @@ flare_count = df['shape'].value_counts()["flare"]
 changed_shape_count = df['shape'].value_counts()["changed"]
 other_shape_count = df['shape'].value_counts()["other"]
 
+#group all shapes, sort, create labels
 all_shapes = [sphere_count, cone_count, diamond_count, flare_count, changed_shape_count, other_shape_count]
-plt.plot(all_shapes)
+all_shapes_sorted = all_shapes.sort(reverse=True)
+shape_names = ["Sphere", "Cone", "Diamond", "Flare", "Changed Shape", "Other"]
 
+#create and format pie chart
+plt.style.use("ggplot")
+plt.bar(shape_names, all_shapes)
+
+plt.title("Reported Shapes of UFOs (1906-2014)")
+plt.xlabel("Reported Shape")
+plt.ylabel("Number of Reports")
 
 plt.show()
-
